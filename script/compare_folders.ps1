ee97893bab86782ee24ab7e90d4d8ea75d4cc031
Aleksandr Smolyakov <v-alsmo@microsoft.com>
2020-12-10 16:38:49 +0300

    Update compare_folders.ps1

--- script/compare_folders.ps1
+++ script/compare_folders.ps1
@@ -1,17 +1,25 @@
 param ($pathToGeneratedArtifact, $pathToDownloadedArtifact, $reference, $difference);
 
-$folderReference = -join($pathToGeneratedArtifact, $reference); 
-$folderDifference = -join($pathToDownloadedArtifact, $difference);
-
-$FolderReferenceContents = Get-ChildItem $folderReference -Recurse | where-object {-not $_.PSIsContainer}
-$FolderDifferenceContents = Get-ChildItem $folderDifference -Recurse | where-object {-not $_.PSIsContainer}
-
-$CheckResult = Compare-Object -ReferenceObject $FolderReferenceContents -DifferenceObject $FolderDifferenceContents -Property ('Name', 'Length');
-
-if ($CheckResult) {
-    Write-Host Folders are differs
-    Write-Host $CheckResult
+try {
+    $folderReference = -join($pathToGeneratedArtifact, $reference); 
+    $folderDifference = -join($pathToDownloadedArtifact, $difference);
+    
+    $FolderReferenceContents = Get-ChildItem $folderReference -Recurse | where-object {-not $_.PSIsContainer}
+    $FolderDifferenceContents = Get-ChildItem $folderDifference -Recurse | where-object {-not $_.PSIsContainer}
+    
+    $CheckResult = Compare-Object -ReferenceObject $FolderReferenceContents -DifferenceObject $FolderDifferenceContents -Property ('Name', 'Length');
+    
+    if ($CheckResult) {
+        Write-Host Folders are differs
+        Write-Host $CheckResult
+        Write-Host "##vso[task.complete result=Failed] Check failed"
+    } else {
+        Write-Host "##vso[task.complete result=Succeeded;] Check passed"
+    }
+} catch {
+    Write-Host "An error occurred:"
+    Write-Host $_
     Write-Host "##vso[task.complete result=Failed] Check failed"
-} else {
-    Write-Host "##vso[task.complete result=Succeeded;] Check passed"
 }
+
+exit $LASTEXITCODE
