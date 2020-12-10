f4e3f53c49e21b1b03427c49c7db6b5b7a19def5
Aleksandr Smolyakov <v-alsmo@microsoft.com>
2020-12-10 16:53:31 +0300

    update

--- script/compare_folders.ps1
+++ script/compare_folders.ps1
@@ -6,15 +6,16 @@ try {
     
     $FolderReferenceContents = Get-ChildItem $folderReference -Recurse | where-object {-not $_.PSIsContainer}
     $FolderDifferenceContents = Get-ChildItem $folderDifference -Recurse | where-object {-not $_.PSIsContainer}
-    
+
     $CheckResult = Compare-Object -ReferenceObject $FolderReferenceContents -DifferenceObject $FolderDifferenceContents -Property ('Name', 'Length');
     
     if ($CheckResult) {
-        Write-Host Folders are differs
+        Write-Host "Folders are differs"
         Write-Host $CheckResult
         Write-Host "##vso[task.complete result=Failed] Check failed"
     } else {
-        Write-Host "##vso[task.complete result=Succeeded;] Check passed"
+        echo "Folders are identical "
+        echo "Check passed"
     }
 } catch {
     Write-Host "An error occurred:"
