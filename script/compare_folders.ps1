520ba905c5c267acebd1470ac3154c7157c6926e
Aleksandr Smolyakov <v-alsmo@microsoft.com>
2020-12-23 00:25:02 +0300

    update

--- script/compare_folders.ps1
+++ script/compare_folders.ps1
@@ -15,7 +15,7 @@ param (
 )
 
 try { 
-    Write-Host "##[command] Compare folder $folderReference with $folderDifference"
+    Write-Host "##[command]Compare folder $folderReference with $folderDifference"
 
     $FolderReferenceContents = Get-ChildItem $folderReference -Recurse | where-object { -not $_.PSIsContainer }
     $FolderDifferenceContents = Get-ChildItem $folderDifference -Recurse | where-object { -not $_.PSIsContainer }
@@ -23,22 +23,21 @@ try {
     $CheckResult = Compare-Object -ReferenceObject $FolderReferenceContents -DifferenceObject $FolderDifferenceContents -Property ('Name', 'Length');
 
     if ($CheckResult) {
+        Write-Host "##[error]Folders are differs - Check failed"
+        Write-Host $CheckResult
+
         Write-Output "Reference folder:"
-        Tree "$folderReference" /F | Select-Object -Skip 2 
+        Tree "$folderReference" /F | Select-Object -Skip 2
 
         Write-Output "Difference folder:"
-        Tree "$folderDifference" /F | Select-Object -Skip 2 
-        
-        Write-Host "Folders are differs"
-        Write-Host $CheckResult
-        Write-Host "##vso[task.logissue type=error;] Check failed"
+        Tree "$folderDifference" /F | Select-Object -Skip 2
     }
     else {
-        Write-Output "##[section] Folders are identical - Check passed"
+        Write-Output "##[section]Folders are identical - Check passed"
     }
 }
 catch {
-    Write-Host "##[error] An error occurred:"
+    Write-Host "##[error]An error occurred:"
     Write-Host $_
     Write-Host "##vso[task.complete result=Failed] Check failed"
 }
